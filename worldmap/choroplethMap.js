924f118b5111b9eb5cf404f0ed52dc25d337a2ed
vinluanmn <144140089+vinluanmn@users.noreply.github.com>
2024-01-18 21:08:01 -0500

    Add Choropleth Layers
    combined choroplethmap and choroplethchgmap into layers  on on js script

--- worldmap/choroplethMap.js
+++ worldmap/choroplethMap.js
@@ -1,29 +1,28 @@
-// ccreate map centered on Charllote, NC - [35.2271, 80.8431]
+// create map centered on Charllote, NC - [35.2271, 80.8431]
 let myMap = L.map("map", {
     center: [35.2271, -80.8431],
     zoom: 3
 });
 
 // add the street layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// load populationData.csv and countries.geojson
-let path = "populationData.csv"
+    // load populationData.csv and countries.geojson
+let path = "populationchg.csv"
 let geoJSON = "countries.geojson"
 
+// Load csv and geojson and combine
 Promise.all([
     d3.csv(path),
-    d3.json(geoJSON)
-]).then(([csvData, geoJSONData]) => {
-    //console.log(csvData);
-    //console.log(geoJSONData);
-
-    // Create key value pair from country name to CSV data
-    let csvDataMap = new Map();
-    csvData.forEach(row => {
-        //'Country Code' is common key in CSV data
+    d3.json(geoJSON)]).then(([csvData, geoJSONData]) => {
+        //console.log(csvData);
+        //console.log(geoJSONData);
+        //Create key value pair from country name to CSV data
+        let csvDataMap = new Map();
+        csvData.forEach(row => {
+            //'Country Code' is common key in CSV data
         csvDataMap.set(row['Country Code'], row);
     });
 
@@ -44,15 +43,15 @@ Promise.all([
     // variable for max and min population
     let maxPopulation = Math.max(...populationValues);
     let minPopulation = Math.min(...populationValues);
-    console.log(populationValues);
-    console.log(maxPopulation);
-    console.log(minPopulation);
+    //console.log(populationValues);
+    //console.log(maxPopulation);
+    //console.log(minPopulation);
     console.log(geoJSONData);
 
         // make a choropleth data layer
         let choroLayer = L.choropleth(geoJSONData, {
             // choose the property to create the gradients for the choropleth
-                // "2022 [YR2022]" indicates the population for the year 2022
+                // [2022] indicates the population for the year 2022
             valueProperty: [2022],
 
             // establish the range of the color scale for the choropleth 
@@ -79,18 +78,48 @@ Promise.all([
             {
                 layer.bindPopup("<b>Country</b>: " + feature.properties.ADMIN + "<hr>" + "<b>Population: </b>" + feature.properties[2022]);
             }
+        });
 
-        }).addTo(myMap);
-        
         console.log(choroLayer)
 
-        // set up the legend
-        let legend = L.control(
-            {position: "bottomright"}
-        );
+        // Define variables for Population Change Layer group
+        let choroChgLayer = L.choropleth(geoJSONData, {
+            // choose the property to create the gradients for the choropleth
+            // "Change" indicates the change in population for the past 10 years
+            valueProperty: ['Change'],
+            // establish the range of the color scale for the choropleth 
+            scale: ["#bd3737", "#3769bd"],
+            
+            // establish the number of breaks (blocks) in the step range on the scale
+            steps: 10,
+            // establish a mode of "q" so that the breaks are distributed based
+            // on quantiles
+            mode: "q",
+            // add a style attribute that will show an outline on the countries 
+            style: {
+                // border attributes
+                color: "#FFFFFF", // white border
+                weight: 1.0,
+                fillOpacity: 0.75},
+                // use onEachFeature to get the name of the country and the change data
+                // then bind the info to the popups for each county in the map
+            onEachFeature: function(feature, layer)
+            {
+                layer.bindPopup("<b>Country</b>: " + feature.properties.ADMIN + "<hr>" + "<b>Population Change Past 10 Years: </b>" + feature.properties['Change']);
+            }
+        });
+
+        console.log(choroChgLayer);
+
+        // Define variables for layers
+        let populationLayer = L.layerGroup([choroLayer]);
+        let populationChgLayer = L.layerGroup([choroChgLayer]);
+
+        let populationLegend = L.control({position: 'bottomright'});
+        let populationChgLegend = L.control({position: 'bottomright'});
 
         // use .onAdd to add properties to the legend
-        legend.onAdd = function(){
+        populationLegend.onAdd = function(){
             // draw the legend onto the map by adding a div to the page dynamically
             let div = L.DomUtil.create("div", "info legend");
 
@@ -126,7 +155,63 @@ Promise.all([
             return div;
         };
 
-         //add the legend to the map
-        legend.addTo(myMap);
+// use .onAdd to add properties to the legend
+populationChgLegend.onAdd = function(){
+    // draw the legend onto the map by adding a div to the page dynamically
+    let div = L.DomUtil.create("div", "info legend");
+
+    // to establish the limits, reference the choropleth layer (choroLayer),
+    // and use the valueProperty value to get a value named limits
+    let limits = choroChgLayer.options.limits;
+
+    // to establish the colors on the legend, reference the choropleth layer
+    // (choroLayer) and use the scale property to get a value named colors
+    let colors = choroChgLayer.options.colors;
+
+    // create an empty array to hold the blocks that represent the gradients
+    let labels = [];
+
+    // adding the maximum and minimum
+    let legendInfo = "<h1>Change Past 10 Years</h1>" +
+    "<div class=\"labels\">" +
+      "<div class=\"min\">" + limits[0] + "</div>" +
+      "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    "</div>";
+
+    // to add the legendInfo
+    div.innerHTML = legendInfo;
+
+    // make an ul of the limits
+    limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+      });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+
+    // return the div so that its added to the map
+    return div;
+};
+
+// add the legend to the map
+populationLegend.addTo(myMap);
+
+//overlays that can be toggled on or off
+let overlayMap = {
+    "2022 Population": populationLayer,
+    "2013 - 2022 Population Change": populationChgLayer
+};
+
+myMap.on('overlayadd', function (eventLayer) {
+    if (eventLayer.name === "2022 Population") {
+        this.removeControl(populationChgLegend);
+        populationLegend.addTo(this);
+    } else {
+        this.removeControl(populationLegend);
+        populationChgLegend.addTo(this);
     }
-);
+});
+
+// Pass our map layers into our layer control.
+// Add the layer control to the map.
+L.control.layers({"Base Map": basemap}, overlayMap).addTo(myMap);
+});
