e604eaab40c4426de54daed4cea786230d58bd2e
davecb90 <bartdc9@gmail.com>
2024-01-21 22:09:50 -0500

    chart.js, anime.js additions w css sheet

--- countryCharts.js
+++ countryCharts.js
@@ -28,13 +28,15 @@ function init() {
 }
 
 // function to display world population bar graph
-function bar(selection) {
+function bar() {
     // Fetch the csv data
     d3.csv(csvPath).then((data) => {
 
-        // empty arrays for population and country 
-        pop = [];
-        country = [];
+        // create empty arrays as variables
+        var pop = [];
+        var country = [];
+        var yearpop = [];
+        var countryName = [];
 
         // loop through csv to add values to population and country arrays
         for(var i=0; i < data.length-1 ; i++){
@@ -46,29 +48,146 @@ function bar(selection) {
         };
         console.log(pop);
 
-        // Trace for bar graph
-        let trace = [{
-            x: country,
-            y: pop,
-            type: "bar",            
-        }];
+        // create bar chart with chart.js
+        new Chart(
+            document.getElementById('bar'),
+            {
+              type: 'bar',
+              title: '2022 Total Country Populations',
+              data: {
+                labels: country,
+                datasets: [
+                  {
+                    label: 'Total Population',
+                    data: pop
+                  }
+                ]
+              },
+              // configuration
+              options:{
+                plugins: {
+                    legend: {
+                    display: false
+                    },
+                    title: {
+                    display: true,
+                    text: '2022 Total Country Populations',
+                    font: {
+                        size: 24
+                    }
+                    }, 
+                        // zoom functionality on the 2022 population chart
+                        zoom: {
+                            // Container for pan options
+                            pan: {
+                                // Boolean to enable panning
+                                enabled: true,
 
-        // set layout
-        let layout = {
-            title: {text: "World 2022 Population by Country",
-                        font: {family: "helvetica",
-                                size: 30}},
-            xaxis: {tickangle: -45,
-                showticklabels: true,
-                type: 'category'
-            }
-        };
+                                drag: {
+                                    enabled: true,
+                                },
+                    
+                                // Panning directions. Remove the appropriate direction to disable
+                                // Eg. 'y' would only allow panning in the y direction
+                                // A function that is called as the user is panning and returns the
+                                // available directions can also be used:
+                                //   mode: function({ chart }) {
+                                //     return 'xy';
+                                //   },
+                                mode: 'x',
+                    
+                                rangeMin: {
+                                    // Format of min pan range depends on scale type
+                                    x: null,
+                                    y: null
+                                },
+                                rangeMax: {
+                                    // Format of max pan range depends on scale type
+                                    x: null,
+                                    y: null
+                                },
+                    
+                                // On category scale, factor of pan velocity
+                                speed: 5,
+                    
+                                // Minimal pan distance required before actually applying pan
+                                threshold: 10,
+                    
+                                // Function called while the user is panning
+                                //onPan: function({chart}) { console.log(`I'm panning!!!`); },
+                                // Function called once panning is completed
+                               // onPanComplete: function({chart}) { console.log(`I was panned!!!`); }
+                            },
+                    
+                            // Container for zoom options
+                            zoom: {
+                                // Boolean to enable zooming
+                                wheel: {
+                                    enabled: true
+                                },
+                                    
+                    
+                                // Enable drag-to-zoom behavior
+                                drag: {
+                                    enabled: false
+                                },
+                    
+                                // Drag-to-zoom effect can be customized
+                                // drag: {
+                                //      borderColor: 'rgba(225,225,225,0.3)'
+                                //      borderWidth: 5,
+                                //      backgroundColor: 'rgb(225,225,225)',
+                                //      animationDuration: 0
+                                // },
+                    
+                                // Zooming directions. Remove the appropriate direction to disable
+                                // Eg. 'y' would only allow zooming in the y direction
+                                // A function that is called as the user is zooming and returns the
+                                // available directions can also be used:
+                                //   mode: function({ chart }) {
+                                //     return 'xy';
+                                //   },
+                                mode: 'x',
+                    
+                                rangeMin: {
+                                    // Format of min zoom range depends on scale type
+                                    x: null,
+                                    y: null
+                                },
+                                rangeMax: {
+                                    // Format of max zoom range depends on scale type
+                                    x: null,
+                                    y: null
+                                },
+                    
+                                // Speed of zoom via mouse wheel
+                                // (percentage of zoom on a wheel event)
+                                speed: 70,
+                    
+                                // Minimal zoom distance required before actually applying zoom
+                                threshold: 2,
+                    
+                                // On category scale, minimal zoom level before actually applying zoom
+                                sensitivity: 3,
+                    
+                                // Function called while the user is zooming
+                                //onZoom: function({chart}) { console.log(`I'm zooming!!!`); },
+                                // Function called once zooming is completed
+                               // onZoomComplete: function({chart}) { console.log(`I was zoomed!!!`); }
+                            }
+                        }
+                    }
+                    
+                    
+                }
+              }
             
-        // plot bar chart
-        Plotly.newPlot("bar", trace, layout);
+          );
+       
     });
 }
 
+var myBarChart;
 // function to display individual country bar graph
 function countryBar(selection) {
     
@@ -108,47 +227,56 @@ function countryBar(selection) {
         let tracex = ["2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"];
         let tracey = [first.y2013, first.y2014, first.y2015, first.y2016, first.y2017, first.y2018, first.y2019, first.y2020, first.y2021, first.y2022];
         
-        // Trace for the data for the horizontal bar chart
-        let trace = [{
-            x: tracex,
-            y: tracey,
-            type: "bar",            
-        }];
+        //call update bar chart function to create new bar chart on country change from dropdown selection
+        updateBarChart(selection, tracex, tracey)
+    
+        });        
 
-        // set layout
-        let layout = {
-            title: {
-                text:  `${selection} Population from 2013 - 2022`,
-                font: {
-                    family: "helvetica",
-                    size: 30
-                        }},
-            yaxis: {
-                title: "Population",
-                font: {
-                    family: "helvetica",
-                    size: 30
-            }},
-            xaxis: {
-                title: "Year",
-                font: {
-                    family: "helvetica",
-                    size: 30
-                },
-                tickangle: -45,
-                showticklabels: true,
-                type: 'category',
+};
+
+// Function to update the bar chart
+function updateBarChart(selection, tracex, tracey) {
+    // Check if a chart instance already exists
+    if (myBarChart) {
+        // If it does, destroy the existing chart
+        myBarChart.destroy();
+    }
+
+    // Create a new chart on the same canvas
+    myBarChart = new Chart(
+        document.getElementById('countryBar'),
+        {
+            type: 'bar',
+            title: '2022 Total Country Populations',
+            data: {
+                labels: tracex,
+                datasets: [
+                    {
+                        data: tracey
+                    }
+                ]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: `${selection} Population from 2013 - 2022`,
+                        font: {
+                            size: 24
+                        }
+                    }
+
+                }
             }
-
-        };
-            
-        // plot bar chart
-        Plotly.newPlot("countryBar", trace, layout);
-    });
+        }
+    );
 }
 
-// function for demographics panel
 
+// function for demographics panel
 function demographics(selection) {
     // Fetch the csv data
     d3.csv(csvPath).then((data) => {
